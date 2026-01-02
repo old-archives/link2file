60a4b5ad57d75eeb087a68fc745e9730b668e6fa
old-archives <anshkatiyar6387@gmail.com>
2026-01-02 19:11:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 API_ID = 123456  # api id
 API_HASH = "ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = "8569480032:AAE6E62Phmf5bUCs5tlYDJwuJw3CZ0vGz3s"  # bot token
 
 
 # REDIS
